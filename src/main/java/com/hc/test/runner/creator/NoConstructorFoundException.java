222bd6298b01d72715e65c36b8f491dfb8f8e867
han.congcong <han.congcong@immomo.com>
2019-04-08 21:25:43 +0800

    填代码

---
+++ src/main/java/com/hc/test/runner/creator/NoConstructorFoundException.java
@@ -0,0 +1,8 @@
+package com.hc.test.runner.creator;
+
+public class NoConstructorFoundException extends Exception {
+    public NoConstructorFoundException(){
+        super();
+    }
+
+}
--- src/main/java/com/hc/test/runner/creator/ReflectObjectFactory.java
+++ src/main/java/com/hc/test/runner/creator/ReflectObjectFactory.java
@@ -1,9 +1,12 @@
 package com.hc.test.runner.creator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Preconditions;
 import com.hc.test.runner.ConstructParam;
 import com.hc.test.runner.ValueWapper;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,26 +22,61 @@ public class ReflectObjectFactory<T> implements IObjectCreator<T,ConstructParam>
     @Override
     public T create(ConstructParam param) throws Exception {
         String className = param.getClassName();
+        Preconditions.checkNotNull(className,"reflect creator require class name");
+
         ValueWapper[] vw = param.getParam();
         List mp = new ArrayList();
         //todo:传null怎么办
         List<Class> argType = new ArrayList();
+
         if(vw != null){
             for(ValueWapper o : vw){
                 Object p = getParam(o);
-                argType.add(p.getClass());
+                if(p == null){
+                    argType.add(null);
+                }else {
+                    argType.add(p.getClass());
+                }
                 mp.add(p);
             }
         }
-        return (T)Class.forName(className).getConstructor(argType.toArray(new Class[0])).newInstance(mp.toArray());
+        return create(className,argType,mp);
     }
 
-    private Object getParam(ValueWapper o) throws Exception {
-        if(o.isConstructParam()){
-            ConstructParam param = mapper.convertValue(o,ConstructParam.class);
+    private T create(String className, List<Class> argType, List mp) throws Exception {
+        Class clazz = Class.forName(className);
+        Constructor[] constructors = clazz.getConstructors();
+        Constructor needed = null;
+        for (int i = 0; i < constructors.length; i++) {
+            Constructor constructor = constructors[i];
+            Class[] paramTypes = constructor.getParameterTypes();
+            if(paramTypes.length != argType.size()){
+                continue;
+            }
+            for (int m = 0; m < argType.size(); m++) {
+                if(argType.get(i) != null){
+                   if(argType.get(i) != paramTypes[i]){
+                       continue;
+                   }
+                }
+            }
+            needed = constructor;
+        }
+        if(needed != null) {
+            return (T)needed.newInstance(mp.toArray());
+        }
+        throw new NoConstructorFoundException();
+    }
+
+    private Object getParam(ValueWapper wapper) throws Exception {
+        if(wapper == null){
+            return null;
+        }
+        if(wapper.isConstructParam()){
+            ConstructParam param = mapper.convertValue(wapper,ConstructParam.class);
             return defaultObjectFactory.create(param);
         }else{
-            return o.getValue();
+            return wapper.getValue();
         }
     }
 }
