6ebafdb741dfb697d44901ee2280fd06dae8cda2
han.congcong <han.congcong@immomo.com>
2019-04-08 21:40:25 +0800

    增加spring bean 获取

---
+++ src/main/java/com/hc/test/runner/creator/NoNameForSpringBeanException.java
@@ -0,0 +1,7 @@
+package com.hc.test.runner.creator;
+
+public class NoNameForSpringBeanException extends Exception {
+    public NoNameForSpringBeanException(String s){
+        super(s);
+    }
+}
--- src/main/java/com/hc/test/runner/creator/SpringObjectFactory.java
+++ src/main/java/com/hc/test/runner/creator/SpringObjectFactory.java
@@ -1,11 +1,22 @@
 package com.hc.test.runner.creator;
 
 import com.hc.test.runner.ConstructParam;
+import com.hc.test.runner.ValueWapper;
+import org.springframework.context.ApplicationContext;
 
 public class SpringObjectFactory<T> implements IObjectCreator<T,ConstructParam> {
 
+    private ApplicationContext applicationContext;
     @Override
-    public T create(ConstructParam param) {
-        return null;
+    public T create(ConstructParam param) throws NoNameForSpringBeanException {
+        ValueWapper[] wappers = param.getParam();
+        if(wappers == null || wappers.length == 0){
+            throw new NoNameForSpringBeanException("bean name is null");
+        }
+        Object bean =  applicationContext.getBean(wappers[0].toString());
+        if(bean == null){
+            throw new NoNameForSpringBeanException("bean name is " + wappers[0].toString());
+        }
+        return (T)bean;
     }
 }
