e74c8c8ab2183066b8831a2cd86c467b58507d36
han.congcong <han.congcong@immomo.com>
2019-06-03 17:19:23 +0800

    端口

--- src/main/java/com/hc/test/server/TestServer.java
+++ src/main/java/com/hc/test/server/TestServer.java
@@ -2,13 +2,14 @@ package com.hc.test.server;
 
 import org.eclipse.jetty.server.Server;
 
-public class TestServer {
+public class TestServer{
+
+    private static final int port = 9999;
 
     public static void main(String[] args) throws Exception {
         new TestServer().start();
     }
     public void start() throws Exception {
-        int port = 9999;
         Server server = new Server(port);
         server.start();
     }
