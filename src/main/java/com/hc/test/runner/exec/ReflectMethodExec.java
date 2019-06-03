e0532e05d9a03188a8ff4f63fab1e836138c46e0
han.congcong <han.congcong@immomo.com>
2019-06-03 20:43:50 +0800

    modify return value

--- src/main/java/com/hc/test/runner/exec/ReflectMethodExec.java
+++ src/main/java/com/hc/test/runner/exec/ReflectMethodExec.java
@@ -1,6 +1,8 @@
 package com.hc.test.runner.exec;
 
+
 import java.lang.reflect.InvocationTargetException;
+import java.util.stream.Stream;
 
 /**
  * 使用反射机制执行
@@ -8,15 +10,33 @@ import java.lang.reflect.InvocationTargetException;
 public class ReflectMethodExec implements IMethodExec {
 
     @Override
-    public Object exec(ExecParam param) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Object instant = param.getOrigin();
-        String methodNamd = param.getMethodName();
-        Object[] args = param.getParams();
-        Class[] argTypes = new Class[args.length];
-        for (int i = 0; i < args.length; i++) {
-            argTypes[i] = args[i].getClass();
-        }
+    public Object exec(ExecParam param) {
+        Class[] argTypes = null;
+        try {
+            Object instant = param.getOrigin();
+            String methodName = param.getMethodName();
+            Object[] args = param.getParams();
+            argTypes = new Class[args.length];
+            for (int i = 0; i < args.length; i++) {
+                argTypes[i] = args[i].getClass();
+            }
 
-        return instant.getClass().getMethod(methodNamd,argTypes).invoke(instant,args);
+            return instant.getClass().getMethod(methodName, argTypes).invoke(instant, args);
+        }catch (NoSuchMethodException e){
+            return "class " + param.getOrigin().getClass().getName()
+                    + " have no method "+ param.getMethodName()
+                    + " with param class"
+                    + " " + Stream.of(argTypes).map(a -> a.getName())
+                    .reduce((a,b)-> a+b);
+        }catch (IllegalAccessException e){
+            return "class " + param.getOrigin().getClass().getName()
+                    + " method "+ param.getMethodName()
+                    + " with param class"
+                    + " " + Stream.of(argTypes).map(a -> a.getName())
+                    .reduce((a,b)-> a+b)
+                    + "can not access from outside";
+        }catch(InvocationTargetException e){
+            return e;
+        }
     }
 }
