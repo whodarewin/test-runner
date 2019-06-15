9e79c2d79233b502dd58a6b44e6a519de2edf6d6
han.congcong <han.congcong@immomo.com>
2019-06-15 21:17:12 +0800

    add search button

---
+++ src/main/java/com/hc/test/server/model/TreeNode.java
@@ -0,0 +1,12 @@
+package com.hc.test.server.model;
+
+/**
+ * TreeNode
+ *
+ * @author han.congcong
+ * @date 2019/6/10
+ */
+
+public class TreeNode {
+
+}
--- src/test/java/com/hc/test/runner/scanner/ClassScannerTest.java
+++ src/test/java/com/hc/test/runner/scanner/ClassScannerTest.java
@@ -8,7 +8,9 @@ import java.util.Set;
  * Created by congcong.han on 2019/6/7.
  */
 public class ClassScannerTest extends TestCase{
+
     private Scanner classScanner = new Scanner();
+
     public void testClassScanner() throws ClassNotFoundException {
         Set<String> classes = classScanner.scanAllClasses("com.hc.test.runner.scanner");
         assertTrue(classes.contains("com.hc.test.runner.scanner.Scanner"));
